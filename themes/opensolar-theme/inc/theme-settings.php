78d0d105d07ff47e52ac5275fe32ffd9da79bcca
Oleksii Tsioma <oleksiitsioma@Oleksiis-MacBook-Pro-New.local>
2023-07-23 00:50:50 -0700

    Enabling Post Thumbnails

--- themes/opensolar-theme/inc/theme-settings.php
+++ themes/opensolar-theme/inc/theme-settings.php
@@ -5,6 +5,7 @@ add_action( 'after_setup_theme' , '___twf_theme_supports');
 function ___twf_theme_supports(){
 
 	add_theme_support( 'menus' );
+	add_theme_support( 'post-thumbnails', array( 'post' ) );  
 	add_theme_support( 'custom-logo', [
 		'height'                => 120,
 		'width'                 => 520,
