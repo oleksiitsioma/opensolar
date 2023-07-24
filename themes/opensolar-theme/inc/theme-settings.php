237bda3706b4d330e8ebcbe55feabe1b66377d99
Oleksii Tsioma <oleksiitsioma@Oleksiis-MacBook-Pro-New.local>
2023-07-24 00:52:55 -0700

    Page title

--- themes/opensolar-theme/inc/theme-settings.php
+++ themes/opensolar-theme/inc/theme-settings.php
@@ -5,7 +5,8 @@ add_action( 'after_setup_theme' , '___twf_theme_supports');
 function ___twf_theme_supports(){
 
 	add_theme_support( 'menus' );
-	add_theme_support( 'post-thumbnails', array( 'post' ) );  
+	add_theme_support( 'post-thumbnails', array( 'post' ) );
+	add_theme_support( 'title-tag' );
 	add_theme_support( 'custom-logo', [
 		'height'                => 120,
 		'width'                 => 520,
@@ -16,3 +17,6 @@ function ___twf_theme_supports(){
 
 }
 
+add_filter( 'document_title_separator', function(){
+	return ' | ';
+} );
