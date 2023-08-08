da79c12b8449ffc717a4c13aa8d6d7ec0ca60565
Oleksii Tsioma <oleksiitsioma@Oleksiis-MacBook-Pro-New.local>
2023-08-07 21:07:26 -0700

    Disable layout restrain for news frontend

--- themes/opensolar-theme/single-news.php
+++ themes/opensolar-theme/single-news.php
@@ -1,26 +1,22 @@
 <?php get_header(); ?> 
 
-<div class="container colGr">
-    <div class="colGr__col_6">
-        <div class="postContent">
+<div class="postContent">
 
-            <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+    <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 
-            <!-- <h1 class="singleNews__title"><?php the_title(); ?></h1> -->
+    <!-- <h1 class="singleNews__title"><?php the_title(); ?></h1> -->
 
-                
-                
-                <?php 
-                
-                the_content();
-                
-                endwhile; endif; 
+        
+        
+        <?php 
+        
+        the_content();
+        
+        endwhile; endif; 
 
-            ?>
-        </div>
-    </div>
+    ?>
+    
 </div>
-
 
 <?php 
 
