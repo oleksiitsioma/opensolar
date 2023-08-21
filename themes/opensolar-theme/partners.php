d940d25d424843fc0a53186bd62ca0e0c3e2391e
Oleksii Tsioma <oleksiitsioma@Oleksiis-MacBook-Pro-New.local>
2023-08-21 10:50:06 -0700

    Refactoring partners page

---
+++ themes/opensolar-theme/partners.php
@@ -0,0 +1,61 @@
+<?php
+
+// Template name: Partners
+
+get_header();
+
+echo get_template_part( 'template-parts/loop' );
+
+?>
+
+<div class="partnersFilter">
+
+    <?php 
+
+        $productTerms = get_terms([
+            'taxonomy'      => 'partners-products',
+            'hide_empty'    => true
+        ] );
+
+        if( sizeof( $productTerms) > 0){
+
+            echo '<select name="partners-filter-products">';
+            echo '<option disabled selected>Products</option>';
+
+            foreach ( $productTerms as $term ){
+
+                echo '<option value="' . $term->slug . '">' . $term->name . '</option>';
+
+            }
+
+            echo '</select>'; 
+
+        } 
+
+    ?>
+
+</div>
+
+<div class="partnersQuery">
+    <?php
+    
+    $queryArgs = array(
+        'post_type'         => 'partners',
+    );
+
+    $query = new WP_Query( $queryArgs );
+
+    if( $query->have_posts()) : while( $query->have_posts()) : $query->the_post();
+
+        the_title(  );
+        echo '<br>';
+
+    endwhile; wp_reset_postdata(); endif;
+    
+    ?>
+</div>
+
+
+
+
+<?php get_footer(); 
