d644eade3d9876abe46bcaccafd2e896f49e36f2
Oleksii Tsioma <oleksiitsioma@Oleksiis-MacBook-Pro-New.local>
2023-08-21 01:26:23 -0700

    Header Widget area introduction #2

--- themes/opensolar-theme/inc/theme-widgets.php
+++ themes/opensolar-theme/inc/theme-widgets.php
@@ -18,19 +18,6 @@ function ___twf_register_sidebars(){
 	) );
 
 	register_sidebar( array(
-		'name'          => 'Header Buttons Menu',
-		'id'            => "sidebar-header-buttons",
-		'description'   => '',
-		'class'         => '',
-		'before_widget' => '',
-		'after_widget'  => "",
-		'before_title'  => "",
-		'after_title'   => "",
-		'before_sidebar' => '<div class="headerWidget headerWidget_buttons">',
-        'after_sidebar'  => '</div>'
-	) );
-
-	register_sidebar( array(
 		'name'          => 'Footer',
 		'id'            => "sidebar-footer",
 		'description'   => '',
