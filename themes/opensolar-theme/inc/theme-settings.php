aaca7d4ab03a13a759477def2e284035378f3377
Oleksii Tsioma <oleksiitsioma@Oleksiis-MacBook-Pro-New.local>
2023-08-22 20:19:49 -0700

    Refactoring partners page #11

--- themes/opensolar-theme/inc/theme-settings.php
+++ themes/opensolar-theme/inc/theme-settings.php
@@ -5,7 +5,7 @@ add_action( 'after_setup_theme' , '___twf_theme_supports');
 function ___twf_theme_supports(){
 
 	add_theme_support( 'menus' );
-	add_theme_support( 'post-thumbnails', array( 'post', 'page', 'webinars' ) );
+	add_theme_support( 'post-thumbnails', array( 'post', 'page', 'webinars', 'partners', 'news' ) );
 	add_theme_support( 'title-tag' );
 	add_theme_support( 'custom-logo', [
 		'height'                => 120,
